354d45c6b40d191397870d7d8db4bcb4087ff97b
fabiodesousaleal <fsl2@msn.com>
2021-04-16 12:45:23 -0300

    Implementando API CRUD completo

--- src/Controller/MedicosController.php
+++ src/Controller/MedicosController.php
@@ -1,8 +1,8 @@
 <?php
-
 namespace App\Controller;
 
 use App\Entity\Medico;
+use App\Helper\MedicoFactory;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -10,88 +10,85 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class MedicosController extends AbstractController
 {
     /**
      * @var EntityManagerInterface
      */
     private $entityManager;
+    /**
+     * @var MedicoFactory
+     */
+    private $medicoFactory;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        MedicoFactory $medicoFactory)
     {
-
         $this->entityManager = $entityManager;
+        $this->medicoFactory = $medicoFactory;
     }
-
     /**
      * @Route("/medicos", methods={"POST"})
      */
     public function novo(Request $request): Response
     {
         $corpoRequisicao = $request->getContent();
-        $dadosEmJson = json_decode($corpoRequisicao);
-
-        $medico = new Medico();
-        $medico->crm = $dadosEmJson->crm;
-        $medico->nome = $dadosEmJson->nome;
-
+        $medico = $this->medicoFactory->criarMedico($corpoRequisicao);
         $this->entityManager->persist($medico);
         $this->entityManager->flush($medico);
-
         return new JsonResponse($medico);
-
     }
-
     /**
      * @Route("/medicos", methods={"GET"})
      */
     public function buscarTodos(): Response
     {
-    $repositorioDeMedicos=$this
-        ->getDoctrine()
-        ->getRepository(Medico::class);
-    $medicoList = $repositorioDeMedicos->findAll();
-    return new JsonResponse($medicoList);
-    }
-
-    /**
-     * @Route("/medicos/{id}", methods={"GET"})
-     */
-    public function buscarUm(Request $request ): Response
-    {
-        $id=$request->get('id');
         $repositorioDeMedicos=$this
             ->getDoctrine()
             ->getRepository(Medico::class);
-       $medico=$repositorioDeMedicos->find($id);
-       $codigoRetorno = is_null($medico) ? Response::HTTP_NO_CONTENT : 200;
+        $medicoList = $repositorioDeMedicos->findAll();
+        return new JsonResponse($medicoList);
+    }
+    /**
+     * @Route("/medicos/{id}", methods={"GET"})
+     */
+    public function buscarUm(int $id, Request $request ): Response
+    {
+        $medico = $this->buscaMedico($id);
+        $codigoRetorno = is_null($medico) ? Response::HTTP_NO_CONTENT : 200;
         return new JsonResponse($medico, $codigoRetorno);
     }
-
     /**
      * @Route("/medicos/{id}", methods={"PUT"})
      */
     public function atualizar(int $id, Request $request): Response
     {
         $corpoRequisicao = $request->getContent();
-        $dadosEmJson = json_decode($corpoRequisicao);
+        $medicoEnviado = $this->medicoFactory->criarMedico($corpoRequisicao);
+        $medico = $this->buscaMedico($id);
+        if (is_null($medico)) {
+            return new Response('', Response::HTTP_NOT_FOUND);
+        }
+        $medico->crm = $medicoEnviado->crm;
+        $medico->nome = $medicoEnviado->nome;
 
-        $medicoEnviado = new Medico();
-        $medicoEnviado->crm = $dadosEmJson->crm;
-        $medicoEnviado->nome = $dadosEmJson->nome;
+        $this->entityManager->flush();
+        return new JsonResponse($medico);
+
+    }
+
+    /**
+     * @param $id
+     * @return object|null
+     */
+    public function buscaMedico($id): ?object
+    {
         $repositorioDeMedicos = $this
             ->getDoctrine()
             ->getRepository(Medico::class);
-
-        $medicoExistente = $repositorioDeMedicos->find($id);
-        if (is_null($medicoExistente)) {
-            return new Response('', Response::HTTP_NOT_FOUND);
-        }
-        $medicoExistente->crm = $medicoEnviado->crm;
-        $medicoExistente->nome = $medicoEnviado->nome;
-
-        $this->entityManager->flush();
-        return new JsonResponse($medicoExistente);
-
+        $medico = $repositorioDeMedicos->find($id);
+        return $medico;
     }
 }
